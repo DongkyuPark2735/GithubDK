041293cdaecebcafc1361a91295174c2c035347d
DonhkyuPark2735 <qer2735@gmail.com>
2022-03-14 23:54:20 +0900

    220314_atHome

---
+++ Mar_14_Ex01.java
@@ -0,0 +1,23 @@
+
+public class Mar_14_Ex01 {
+	private final static int APPLE = 1;
+	private final static int PEACH = 2;
+	private final static int BANANA = 3;
+
+	public static void main(String[] args) {
+		
+		int type = APPLE ;
+		switch(type) {
+		case APPLE :
+			System.out.println(57+"kacl");
+			break;
+		case BANANA :
+			System.out.println(34+"kacl");
+			break;
+		case PEACH :
+			System.out.println(93+"kacl");
+			break;
+		}
+	}
+
+}
